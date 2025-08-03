08fa1591cff7c829b329ef0d50a146c9d21ac9a1
HuyDang05 <quanghuy23052005@gmail.com>
2025-08-03 11:32:08 +0700

    chore: install pug

--- index.ts
+++ index.ts
@@ -2,9 +2,11 @@ import express, { Express, Request, Response } from "express";
  const app: Express = express();
   const port: number | string = process.env.PORT || 3000;
 
+  app.set("views", "./views");
+  app.set("view engine", "pug");
 
-  app.get("/", (req: Request, res: Response) => {
-    res.send("hh")
+  app.get("/tours", (req: Request, res: Response) => {
+    res.render("client/pages/tours/index");
   })
 
 
