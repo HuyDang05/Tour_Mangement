d322bee6476b94bb53da868141bcc1ff54c2c9a4
HuyDang05 <quanghuy23052005@gmail.com>
2025-08-04 16:05:35 +0700

    chore: Create Route

---
+++ routes/client/tour.route.ts
@@ -0,0 +1,17 @@
+import  { Router, Request, Response } from "express";
+import Tour from "../../models/tour.model";
+const router : Router = Router();
+
+
+router.get("/", async (req: Request, res: Response) => {
+
+  const tours = await Tour.findAll({
+    raw: true
+  });
+
+  res.render("client/pages/tours/index", {
+    tours: tours
+  });
+});
+
+export const tourRoutes: Router = router
