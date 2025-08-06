fa86ca89e2e39bf7e15417b80b6884c22830c324
HuyDang05 <quanghuy23052005@gmail.com>
2025-08-06 15:38:45 +0700

    feat: Display Cart Page

---
+++ routes/client/cart.route.ts
@@ -0,0 +1,11 @@
+import  { Router } from "express";
+const router : Router = Router();
+
+import * as controller from "../../controllers/client/cart.controller";
+
+router.get("/", controller.index);
+
+
+
+
+export const cartRoutes: Router = router;
--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -1,6 +1,7 @@
 import { Express } from "express";
 import { tourRoutes } from "./tour.route";
 import { categoryRoutes } from "./category.route";
+import { cartRoutes } from "./cart.route";
 
 
 
@@ -8,6 +9,8 @@ import { categoryRoutes } from "./category.route";
 const clientRoutes = (app: Express): void => {
   app.use(`/tours`, tourRoutes);
   app.use(`/categories`, categoryRoutes);
+  app.use(`/cart`, cartRoutes);
+
 
 
 };
